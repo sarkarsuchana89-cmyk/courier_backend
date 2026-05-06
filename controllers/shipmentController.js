01734c540d25249e117bd4a615e133b37373113e
arkobag558-creator <arkobag558@gmail.com>
2026-05-06 12:14:45 +0530

    Shipmentready part 1

--- controllers/shipmentController.js
+++ controllers/shipmentController.js
@@ -43,6 +43,7 @@ exports.createShipment = (req, res) => {
     declared_value = 0,
     remarks,
     return_details,
+    reference_name,
     cust_ref_no,
     sl_no,
     sender,
@@ -62,7 +63,7 @@ exports.createShipment = (req, res) => {
         (awb_number, origin_city_id, destination_city_id, shipment_date,
          pcs, weight, mode, contents, declared_value,
          remarks, return_details, cust_ref_no, sl_no)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,  ?, ?)
       `;
 
       conn.query(shipmentSql, [
@@ -86,15 +87,42 @@ exports.createShipment = (req, res) => {
 
         const addressSql = `
           INSERT INTO shipment_addresses
-          (shipment_id, type, name, address, pincode_id, city_id, district_id, state_id, country_id, phone, whatsapp, email)
-          VALUES ?
+(shipment_id, type, name, reference_name, address, pincode_id, city_id, district_id, state_id, country_id, phone, whatsapp, email)
+VALUES ?
         `;
 
         const values = [
-          [shipmentId, "sender", sender.name, sender.address, sender.pincode_id, sender.city_id, sender.district_id, sender.state_id, sender.country_id, sender.phone, sender.whatsapp, sender.email],
-          [shipmentId, "receiver", receiver.name, receiver.address, receiver.pincode_id, receiver.city_id, receiver.district_id, receiver.state_id, receiver.country_id, receiver.phone, receiver.whatsapp, receiver.email]
-        ];
-
+  [
+    shipmentId,
+    "sender",
+    sender.name,
+    null, // sender no ref (optional)
+    sender.address,
+    sender.pincode_id,
+    sender.city_id,
+    sender.district_id,
+    sender.state_id,
+    sender.country_id,
+    sender.phone,
+    sender.whatsapp,
+    sender.email
+  ],
+  [
+    shipmentId,
+    "receiver",
+    receiver.name,
+    receiver.reference_name || null, // ✅ THIS IS YOUR FIELD
+    receiver.address,
+    receiver.pincode_id,
+    receiver.city_id,
+    receiver.district_id,
+    receiver.state_id,
+    receiver.country_id,
+    receiver.phone,
+    receiver.whatsapp,
+    receiver.email
+  ]
+];
         conn.query(addressSql, [values], (err2) => {
           if (err2) return conn.rollback(() => sendError(res, err2));
 
